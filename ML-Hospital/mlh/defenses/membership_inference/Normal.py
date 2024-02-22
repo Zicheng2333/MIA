e01065ff23135ecdba6541ce9cd4e71a85bcb868
Zicheng2333 <12212609@mail.sustech.edu.cn>
2024-02-22 11:49:18 +0800

    save model and csv file

--- ML-Hospital/mlh/defenses/membership_inference/Normal.py
+++ ML-Hospital/mlh/defenses/membership_inference/Normal.py
@@ -10,8 +10,10 @@ import torch.nn as nn
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
+import csv
+
 class TrainTargetNormal(Trainer):
-    def __init__(self, model, device, num_class, epochs=100, learning_rate=0.01, momentum=0.9, weight_decay=5e-4, smooth_eps=0.8, log_path="./"):
+    def __init__(self, model, model_name, device, num_class, epochs=100, learning_rate=0.01, momentum=0.9, weight_decay=5e-4, smooth_eps=0.8, log_path="./"):
 
         super().__init__()
 
@@ -31,8 +33,22 @@ class TrainTargetNormal(Trainer):
         self.criterion = nn.CrossEntropyLoss()
 
         self.log_path = log_path
-        logx.initialize(logdir=self.log_path,
+
+
+        self.model_name = model_name
+        self.logging_path = os.path.join(self.log_path,self.model_name)
+        logx.initialize(logdir=self.logging_path,
                         coolname=False, tensorboard=False)
+        self.log_file = os.path.join(self.logging_path, 'training_log.csv')
+
+        with open(self.log_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Epoch", 'Total Sample', "Train Loss", "Train Accuracy", "Test Accuracy", 'Total Time'])
+
+    def log_to_csv(self,epoch,total_sample,train_loss,train_acc,test_acc,total_time):
+        with open(self.log_file, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([epoch, total_sample, train_loss, train_acc, test_acc, total_time])
 
     @staticmethod
     def _sample_weight_decay():
@@ -69,6 +85,9 @@ class TrainTargetNormal(Trainer):
         if not os.path.exists(self.log_path):
             os.makedirs(self.log_path)
 
+        if not os.path.exists(self.logging_path):
+            os.makedirs((self.logging_path))
+
         try:
             test_acc = self.eval(test_loader)
             print("Initial test acc:", test_acc)
@@ -95,13 +114,14 @@ class TrainTargetNormal(Trainer):
                 train_acc = self.eval(train_loader)
                 test_acc = self.eval(test_loader)
 
+                self.log_to_csv(e, len(train_loader.dataset), trainLoss, train_acc, test_acc, time.time() - t_start)
+
                 logx.msg('Train Epoch: %d, Total Sample: %d, Train Acc: %.3f, Test Acc: %.3f, Total Time: %.3fs' % (
                     e, len(train_loader.dataset), train_acc, test_acc, time.time() - t_start))
                 self.scheduler.step()
 
-            torch.save(self.model.state_dict(), os.path.join(self.log_path, "%s.pth" % self.model_save_name))
         except KeyboardInterrupt:
-            pass
+            torch.save(self.model.state_dict(), os.path.join(self.log_path, "%s.pth" % self.model_save_name))
 
 
 
