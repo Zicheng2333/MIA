57ee2ad8c3c611cba8d47fa77887beadb5ceb47e
Zicheng2333 <12212609@mail.sustech.edu.cn>
2024-03-09 16:47:11 +0800

    add train acc

--- ML-Hospital/mlh/defenses/membership_inference/pruned_Normal.py
+++ ML-Hospital/mlh/defenses/membership_inference/pruned_Normal.py
@@ -302,6 +302,18 @@ class TrainTargetNormal(Trainer):
                 train_loader=train_loader,
                 test_loader=test_loader
             )
+        elif self.args.mode == 'eval':
+            self.model.eval()
+            ops, params = tp.utils.count_ops_and_params(
+                self.model, example_inputs=example_input,
+            )
+            self.args.logger.info("Params: {:.2f} M".format(params / 1e6))
+            self.args.logger.info("ops: {:.2f} M".format(ops / 1e6))
+            val_acc, val_loss = eval(self.model, test_loader)
+            train_acc, train_loss = eval(self.model,train_loader)
+
+            self.args.logger.info("Train Acc: {:.4f} Train Loss: {:.4f} Val Acc: {:.4f} Val Loss: {:.4f}\n".format(train_acc, train_loss, val_acc, val_loss))
+
 
 
 
